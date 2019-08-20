61b9729537aab02bfc0f27f8f7f6cf2a5ffc141c
Nick Ramsay <48420063+nick-ramsay@users.noreply.github.com>
2019-08-21 09:01:55 +1000

    Clearing of enemyHistory array and final enemyVariable added to newGame() function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         userCharacter = "";
         currentEnemy = "";
         finalEnemy = false;
+        enemyHistory = [];
 
         var obiWan = {
             name: "Obi Wan",
